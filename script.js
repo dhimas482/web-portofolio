1cf814d97af867b45dc4849565e44bb960583252
website <ilhambos3w@gmail.com>
2025-03-27 00:41:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Modal Sertifikat
+const modal = document.getElementById("sertifikatModal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
